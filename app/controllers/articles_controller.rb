4e1b06f7962191ee6b8bbe95efcd0537bfeb34b5
FabioVV <fabio-varela@protonmail.com>
2022-09-16 17:35:29 -0300

    more styling to form new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,11 @@ class ArticlesController < ApplicationController
     end
 
     def index
-        @articles = Article.all
+        @articles = Article.all()
     end
 
     def new
-        @article = Article.new
+        @article = Article.new()
     end
 
     def edit
