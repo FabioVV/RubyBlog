b14641d6e8ab038550515f8b2b1720acd45c6729
Fábio Varela <fabio-varela@protonmail.com>
2022-09-21 15:34:31 -0300

    Add users validations and users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_031211) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_180919) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_031211) do
     t.datetime "updated_at", null: false
     t.datetime "just_learning_addtables"
     t.datetime "just_learning_addtablesTWO"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
