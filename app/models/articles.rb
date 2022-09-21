d346c06275cbd1a08bcf503c0fbc042c07608c02
Fábio Varela <fabio-varela@protonmail.com>
2022-09-21 15:49:07 -0300

    Add one-to-many association between users and articles

--- app/models/articles.rb
+++ app/models/articles.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
 
+    belongs_to :user
+
     validates :title, presence: true, length: { minimum: 6 }
     validates :description, presence:true, length: { maximum: 300 }
 
