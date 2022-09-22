a0f29e5e2a304edeb96633cfecc5d01620838d43
Fábio Varela <fabio-varela@protonmail.com>
2022-09-22 05:18:26 -0300

    fixed styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
         size = options[:size]
         gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
 
-        image_tag(gravatar_url, alt: user.username, class:"centeri p-1 w-10 h-10 rounded-full ring-2 ring-gray-300 dark:ring-gray-500")
+        image_tag(gravatar_url, alt: user.username, class:"centeri rounded-full ring-2 ring-gray-300 dark:ring-gray-500")
     end
 
 end
