c773b45c5a1fed3e7ddc1fd9ee8cee047846c650
Fábio Varela <fabio-varela@protonmail.com>
2022-09-26 17:52:58 -0300

    added tests, new nav and categories funcionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,38 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except:[:index, :show]
 
     def new
-
+        @category = Category.new
     end
 
     def create
-
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Categoria criada com sucesso!"
+            redirect_to @category
+        else
+        end
     end
 
     def index
-
+        @categories = Category.page params[:page]
     end
 
     def show
+        @category = Category.find(params[:id])
+    end
 
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert] = "Apenas administradores podem criar categorias."
+            redirect_to categories_path
+        end
     end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+
+    
+
     validates :name, presence: true, length:{minimum:8, maximum:25},
     uniqueness: { case_sensitive: false }
+
 end
